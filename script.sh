df0de6177aea006b834aa6bbfb11763ecd618ec0
Ujjwal Kumar <krujjwal0651@gmail.com>
2022-02-03 20:03:45 +0000

    changed script.sh

--- script.sh
+++ script.sh
@@ -2,12 +2,16 @@
 
 NAME=$1
 LNAME=$2
-SHOW=$3
+#SHOW=$3
 
+ echo "Hello $NAME $LNAME, Current time is $(date)"
+
+<<--->
 if [ "$SHOW" = "true" ]
 then
 	echo "Hello $NAME $LNAME, Current time is $(date)"
 else 
 	echo "If you want to see the name, Please MARK the show option"
 fi
+--->
 
